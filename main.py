38419a6f0905137bb1c25a85453d5ad11c606f11
flowers186 <62859441+dvisionst@users.noreply.github.com>
2021-10-07 15:37:39 +0000

    Finished Quiz Game project using OOP principals.

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 # Creating variables in order to loop through the question_data list
 # loop will populate a new list with Question object and respective attributes
 
 question_bank = []
 for dictionary in question_data:
-    text_q = dictionary["text"]
-    text_a = dictionary["answer"]
+    text_q = dictionary["question"]
+    text_a = dictionary["correct_answer"]
     question_bank.append(Question(text_q, text_a))
+
+quiz = QuizBrain(question_bank)
+
+# while loop that will keep populating the next question until there are no more questions left in the set
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print(f"You've completed the quiz.")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
